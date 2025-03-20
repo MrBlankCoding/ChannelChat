acde12713729ce414eb227be131375bc76855052
carlyacobs <160960692+carlyacobs@users.noreply.github.com>
2025-03-20 18:31:31 -0400

    download button

--- ChatApp/static/js/ImageGallery.js
+++ ChatApp/static/js/ImageGallery.js
@@ -13,6 +13,9 @@ class ImageGallery {
       "fixed inset-0 z-50 hidden bg-black/90 flex items-center justify-center";
 
     modal.innerHTML = `
+      <button class="absolute top-4 left-4 text-white hover:text-gray-300 z-50" id="galleryDownload">
+         <i class="fa-solid fa-download"></i>
+      </button>
       <button class="absolute top-4 right-4 text-white hover:text-gray-300 z-50" id="galleryClose">
         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
